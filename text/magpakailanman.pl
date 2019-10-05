5db7ad4baef661e27eb7057e3e313c425985e944
U-USPSECTOR\Paul.Dekker <Paul.Dekker@HZPZ1N2.corp.uspsector.net>
2019-10-05 12:41:44 -0700

    text/ dir added

---
+++ text/magpakailanman.pl
@@ -0,0 +1,20 @@
+
+
+MAGPAKAILANMAN
+
+   root words: kailan + man
+
+   magpakailanman
+   forevermore
+
+   Mamahalin kita magpakailanman.
+   I will love you forevermore.
+
+   Babantayan kita magpakailanman.
+   I will forever watch over you.
+
+   Mabuhay magpakailanman sa paraiso.
+   Live forever in paradise.
+
+   Mabuhay tayo magpakailanman sa paraiso.
+   Let us live forever in paradise.
