50d81de6ffbd009834dafe4a1636f39167b01664
zeezolahat <dekker@yahoo.com>
2021-05-07 12:52:50 -0700

    mo

--- util/affix_counts_by_type.py
+++ util/affix_counts_by_type.py
@@ -7,27 +7,11 @@ _file =  'roots.txt'
 noun_affix_count = defaultdict(int)
 
 for l in open(_file, 'r'):
-  mo=re.search(r'(\w*_\w*) \[noun\]', l)
+  mo=re.search(r'(\S*_\S*) \[noun\]', l)
   #mo=re.search(r'(\w*_\w*) \[(\w+)\]', l)
   if mo and mo.lastindex == 1:
     affix = mo[1]
     noun_affix_count[affix] += 1
 
-for k, v in noun_affix_count.items():
-  print(k, v)
-
-f = open('roots.txt', 'r')
-
-d = f.read()
-all = re.split(r'( _ | _\w+|\w+_ | _\w+_ | [a-zCV]+_[a-z]+ )', d)
-
-c = defaultdict(int)
-for l in all:
-    try:
-        l.index('_')
-        c[l] += 1
-    except:
-        continue
-
-for k, v in c.items():
-    print(v, '\t', k.strip())
+for prefix, count in noun_affix_count.items():
+  print(count, prefix)
