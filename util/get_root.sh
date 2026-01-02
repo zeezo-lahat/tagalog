9698678bab64a8963d9be1e75cbd98f7e4ebd5fa
Mister Buckets <zeezo@example.mil>
2026-01-02 09:15:12 +0800

    mo

--- util/get_root.sh
+++ util/get_root.sh
@@ -8,6 +8,7 @@ else
   exit 1
 fi
 
-lynx -dump "https://www.tagaloglessons.com/dictionary/root.php?root=$f&back=Roots" >.out
+#lynx -dump "https://www.tagaloglessons.com/dictionary/root.php?root=$f&back=Roots" >.out
+lynx -dump "https://www.tagalog.com/dictionary/root-word-$f" >.out
 ./util/parse_roots.py .out
 rm .out
